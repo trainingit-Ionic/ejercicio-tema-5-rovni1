9228afa1154dbce09d4fa49d65facc61fc9f5294
Carlos Herrera Conejero <carherco@gmail.com>
2019-02-08 13:04:04 +0100

    servicio auth y páginas para el ejercicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'home', loadChildren: './pages/public/home/home.module#HomePageModule' },
+  { path: 'clients', loadChildren: './pages/private/clients/clients.module#ClientsPageModule' },
+  { path: 'providers', loadChildren: './pages/private/providers/providers.module#ProvidersPageModule' },
+  { path: 'orders', loadChildren: './pages/private/orders/orders.module#OrdersPageModule' },
+  { path: 'invoices', loadChildren: './pages/private/invoices/invoices.module#InvoicesPageModule' },
 ];
 
 @NgModule({
